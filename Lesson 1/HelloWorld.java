0e1c0c49495aa914e507625062b18c41b797a7c2
MetalCrusaders <46503620+MetalCrusaders@users.noreply.github.com>
2020-10-27 16:51:15 -0400

    Types, Variables, and Operators

---
+++ Lesson 1/HelloWorld.java
@@ -0,0 +1,29 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        /*
+        Program execution here
+        System.out.println("Metal Crusader");
+        System.out.println("Robotics is cool!");
+
+        String name;
+        name = "Akash";
+
+        int age = 18;
+        */
+        String firstLine = "Metal Crusader";
+        String secondLine = "Robotics is cool!";
+        System.out.println(firstLine);
+        System.out.println(secondLine);
+    }
+}
+
+// Type is a value that can be stored and changed
+
+// boolean: true or false
+// int: integer (3, -7, 221)
+// double: 2.5, -0.6, 253.935, 3.00
+// String: "hello", "Metal Crusader"
+
+
+// Variables are places that you store types
+
