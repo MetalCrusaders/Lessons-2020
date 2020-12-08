25c7df6eda307040a65d1da79fce4f214c11b1f8
MetalCrusaders <46503620+MetalCrusaders@users.noreply.github.com>
2020-12-08 15:44:25 -0500

    Add files via upload

---
+++ Lesson 1/Methods.java
@@ -0,0 +1,30 @@
+class helloWorld {
+    public static void main(String[] arguments){
+    // Program execution begins here
+    System.out.println("Hello world.");
+
+    String bob;
+    double t = 3.4;
+    double u = 2.5;
+    //int x = (int)3.6;
+    //double y = (double)3/7;
+    bob = "24";
+    System.out.print("24");
+    //Math.sin(x);
+    //Math.pow(2, 3);
+
+    printSquare(5);
+    }
+/*
+    public static double division(double x, double y){
+        return x/y;
+    }
+    
+    public static String method1(String a, int b){
+        return a + b;
+    }
+*/
+    public static void printSquare(int x){
+        System.out.println(x * x);
+    }
+}
